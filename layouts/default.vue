ea7c7442240bc5dcee1a0fc50044e6bfa29a022e
CareTiger <currency@gmail.com>
2022-09-01 21:21:31 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-screen">
+        <Header />
+
+        <div class="border border-red-500">
+            <slot />
+        </div>
+        
+        <Footer />
+    </div>
+</template>
