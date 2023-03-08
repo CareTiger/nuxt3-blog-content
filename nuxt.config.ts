f6182edbe969a4d45772df0b02ada6bec84ce4cf
Ven <CareTiger@users.noreply.github.com>
2023-03-08 11:55:50 -0600

    fix: updated packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    meta: {
-        link: [
-            {
-                href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
-                rel: "stylesheet",
-            },
-            {
-                href: "https://fonts.googleapis.com/css2?family=Material+Icons+Outlined",
-                rel: "stylesheet",
-            },
-        ],
-    },    
-    modules: [
-        '@nuxt/content', '@nuxtjs/tailwindcss'
-    ],
-    content: {
-    // https://content.nuxtjs.org/api/configuration
-    }, 
-})
+  app: {
+    head: {
+      title: "Nuxt 3",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "Nuxt 3" },
+      ],
+      link: [
+        {
+          href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+          rel: "stylesheet",
+        },
+        {
+          href: "https://fonts.googleapis.com/css2?family=Material+Icons+Outlined",
+          rel: "stylesheet",
+        },
+      ],
+    },
+  },
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+});
